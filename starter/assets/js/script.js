0edebef269b5c80422a1f062a1c9b8a2a0fe6bdd
Steva <stevaalex@github.com>
2022-12-04 18:34:36 +0000

    Added array of objects with questions and answers

---
+++ starter/assets/js/script.js
@@ -0,0 +1,43 @@
+var ansAndQues = [ 
+    { 
+        question:"What language is used to form the main body of your code?",
+        choices: ["html", "CSS", "Javascript"],
+        answer: "html"
+     }, 
+    { 
+        question:"What language is used to style your code only?",
+        choices: ["html", "CSS", "Javascript"],
+        answer: "CSS"
+     }, 
+    { 
+        question:"What language is used to form the make your code dynamic and interactive?",
+        choices: ["html", "CSS", "Javascript"],
+        answer: "Javascript"
+     }, 
+    { 
+        question:"In a for loop, how would I increment i by 1? ",
+        choices: ["i++", "i--", "i1"],
+        answer: "i++"
+     }, 
+    { 
+        question:"What would be another way of declaring the variable, var dog = 'woof'; ",
+        choices: ["let dog = 'woof'", "make dog = 'woof'", "say dog = 'woof'"],
+        answer: "let dog = 'woof'"
+     },
+    { 
+        question:"What does DOM stand for? ",
+        choices: ["Data Object Model", "Domain Obsidian Maker", "Document Object Model"],
+        answer: "Document Object Model"
+     },
+     { 
+        question:"In an array named 'arr', the item arr[1] represents which array item?",
+        choices: ["The first item", "The second item", "The third item"],
+        answer: "The second item"
+     },
+     { 
+        question:"In a for loop, how would I increment i by 1?",
+        choices: ["i++", "i--", "i1"],
+        answer: "i--"
+     }
+    
+]
