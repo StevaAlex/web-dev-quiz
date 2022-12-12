f6874ccf508dac1c48106a0dcd5ee79f131b7f47
Steva <stevaalex@github.com>
2022-12-12 23:11:37 +0000

    fuxed storage typo

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,8 +1,9 @@
 
-var userData = JSON.parse(localStorage.getItem("high-scores")); 
+var userData = JSON.parse((localStorage.getItem("high-scores"))); 
+console.log(localStorage.getItem("high-scores"))
 var highscoreEl = document.getElementById("#highscores"); 
 var li1 = document.createElement("li"); ///create li 
 //append li
 console.log("did local store work?" + userData )
-li1.innerHTML = userData;
-highscoreEl.appendChild(li1); 
+highscoreEl.appendChild(li1);  
+li1.innerHTML = userData; 
