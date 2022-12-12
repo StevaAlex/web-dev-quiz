74d867f0f8cf696360a173bc6c2438648e8867e1
Steva <stevaalex@github.com>
2022-12-12 22:42:52 +0000

    Changed local storage code

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,5 +1,3 @@
 
-var userData = window.localStorage.getItem("high-scores") 
-console.log(userData) 
-//find a way to get data from local storage 
-//maybe get data back in a list or array?
+var userData = window.localStorage.getItem("high-scores"); 
+
